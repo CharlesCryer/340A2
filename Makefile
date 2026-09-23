33f5772d70bbdb138c546a5d92e91d82008d95f4
CharlesCryer <161407816+CharlesCryer@users.noreply.github.com>
2026-09-24 11:36:54 +1200

    testfile

--- Makefile
+++ Makefile
@@ -150,6 +150,7 @@ UPROGS=\
 	$U/_uoa_exectime\
 	$U/_uoa_testschd\
 	$U/_used\
+	$U/_mprotecttest\
 
 fs.img: mkfs/mkfs README.xv6 $(UPROGS)
 	mkfs/mkfs fs.img README.xv6 $(UPROGS)
---
+++ user/mprotecttest.c
@@ -0,0 +1,16 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  char* address = sbrk(100);
+  *address = 0x12;
+  mprotect(address);
+  // *address = 0x34;
+  printf("%d\n", (int)(*address));
+  munprotect(address);
+  *address = 0x56;
+  printf("%d\n", (int)(*address));
+  exit(0);
+}
