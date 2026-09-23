709787feac204473a861b14e287d9514e0891784
CharlesCryer <161407816+CharlesCryer@users.noreply.github.com>
2026-09-24 11:44:13 +1200

    smallfix

--- user/user.h
+++ user/user.h
@@ -25,8 +25,8 @@ char* sys_sbrk(int,int);
 int pause(int);
 int uptime(void);
 int getusedmem(void);
-uint64 mprotect(void*);
-uint64 munprotect(void*);
+int mprotect(void*);
+int munprotect(void*);
 
 // ulib.c
 int stat(const char*, struct stat*);
